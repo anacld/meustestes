663ea7e580a30018d55dcc80960f0947657facef
anacld <anacld.rocha@gmail.com>
2021-12-12 20:24:26 -0300

    tratamento de erro

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ app.use('/pedidos', rotaPedidos);
 // tratamento de erro quando nao encontra a rota
 app.use((req, res, next) => {
     const erro = new Error('Página não encontrada!');
-    erro.status(404);
+    erro.status = 404;
     next(erro);
-})
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
 
 module.exports = app;
