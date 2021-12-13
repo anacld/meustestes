06ecd3dd0b6511e49b1468803ea14bcacc3b84ee
anacld <anacld.rocha@gmail.com>
2021-12-12 21:16:03 -0300

    inclusao do bodyparser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const rotaProdutos = require('./routes/produtos');
-app.use('/produtos', rotaProdutos);
-
 const rotaPedidos = require('./routes/pedidos');
+
+app.use(bodyParser.urlencoded({ extended: false})); // apenas dados simples
+app.use(bodyParser.json()); //aceita somente json de entrada no body
+
+app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
 // tratamento de erro quando nao encontra a rota
