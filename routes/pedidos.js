aa27621fc3964deb072baa7899a0925021996573
anacld <anacld.rocha@gmail.com>
2021-12-12 21:23:03 -0300

    ajustes

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -17,7 +17,7 @@ router.post('/', (req, res, next) => {
 
 //RETORNA DADOS DE UM PEDIDO
 router.get('/:id_pedido', (req, res, next) => {
-    const id = req.params.id_pedido
+    const id = req.params.id_pedido;
     res.status(200).send({
         mensagem: 'Detalhes do Pedido',
         id_pedido: id
@@ -28,14 +28,14 @@ router.get('/:id_pedido', (req, res, next) => {
 router.patch('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Pedido alterado!'
-    })
+    });
 });
 
 //EXCLUI UM PEDIDO
 router.delete('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Pedido excluído!'
-    })
+    });
 });
 
 module.exports = router;
--- routes/produtos.js
+++ routes/produtos.js
@@ -7,14 +7,20 @@ router.get('/', (req, res, next) => {
     });
 });
 
+//INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    };
     res.status(201).send({
-        mensagem: 'usando o POST dentro da rota de produtos'
+        mensagem: 'Insere um produto',
+        produtoCriado: produto
     });
 });
 
 router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
+    const id = req.params.id_produto;
 
     if(id === 'especial') {
         res.status(200).send({
@@ -31,13 +37,13 @@ router.get('/:id_produto', (req, res, next) => {
 router.patch('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'usando o PATCH dentro da rota de produtos'
-    })
+    });
 });
 
 router.delete('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'usando o DELETE dentro da rota de produtos'
-    })
+    });
 });
 
 module.exports = router;
