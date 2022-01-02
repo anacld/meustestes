bdfa7f212951da5035e9caa518fc8547380d4635
anacld <anacld.rocha@gmail.com>
2022-01-02 20:24:41 -0300

    deixa publico a imagem

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false})); // apenas dados simples
 app.use(bodyParser.json()); //aceita somente json de entrada no body
 
